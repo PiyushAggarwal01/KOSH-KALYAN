a26b1e6b3979fa9764624e70d0f49ab546b23280
Piyush Aggarwal <Piyush.tech2024@gmail.com>
2025-04-04 14:45:27 +0530

    transs

---
+++ backend/models/transactionns.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const TransactionSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    amount: {
+      type: Number,
+      required: true
+    },
